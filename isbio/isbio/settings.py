1825419e36ef7831b60c4a0c2f4e6feb5845440d
findcomrade <piter.dmitry@gmail.com>
2012-12-05 12:05:01 +0200

    data bese set up completed for the user biouser on localhost

--- isbio/isbio/settings.py
+++ isbio/isbio/settings.py
@@ -11,12 +11,12 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'biodb',                      # Or path to database file if using sqlite3.
+        'USER': 'biouser',                      # Not used with sqlite3.
+        'PASSWORD': 'rna',                  # Not used with sqlite3.
+        'HOST': '/var/run/mysqld/mysqld.sock',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
