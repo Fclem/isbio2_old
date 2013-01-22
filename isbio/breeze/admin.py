841bca20ac3fdc5db7b71bedeaffe6d590da95a3
findcomrade <piter.dmitry@gmail.com>
2013-01-22 15:18:35 +0200

    django admin page is added

---
+++ isbio/breeze/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from breeze.models import Rscripts, Jobs
+
+admin.site.register(Rscripts)
+admin.site.register(Jobs)
--- isbio/isbio/settings.py
+++ isbio/isbio/settings.py
@@ -118,9 +118,9 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap_toolkit',
-    'breeze'
+    'breeze',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
