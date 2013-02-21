d9e3d2792b0eae654377d3c597413e024794733b
findcomrade <piter.dmitry@gmail.com>
2013-02-21 11:24:08 +0200

    django-configurations package used for different SETTINGS.PY configuration

--- isbio/isbio/wsgi.py
+++ isbio/isbio/wsgi.py
@@ -16,12 +16,19 @@ framework.
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "isbio.settings")
+os.environ.setdefault('DJANGO_CONFIGURATION', 'DevSettings')
+
+from configurations.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
+
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
