97d85f97fccaf928bfff2cdebac6ef3aa16d4c64
Dmitry <piter.dmitry@gmail.com>
2013-02-21 13:32:52 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ mkdir ./logs
 echo $0: Installing Dependencies
 source ./venv/bin/activate
 export PIP_REQUIRE_VIRTUALENV=true
-./env/bin/pip install --requirement=./requirements.txt --log=./logs/build_pip_packages.log
+./venv/bin/pip install --requirement=./requirements.txt --log=./logs/build_pip_packages.log
 
 echo $0: BUILD FINISHED
