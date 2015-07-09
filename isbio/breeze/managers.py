b4a7c03df73608f1333c51d22039b0d1b6c37e70
Clement Fiere <clement@fiere.fr>
2015-07-09 16:10:28 +0300

    working version

--- isbio/breeze/managers.py
+++ isbio/breeze/managers.py
@@ -1,69 +1,75 @@
-from django.db.models.query_utils import Q as __originalQ
+from django.db import models
+from django.db.models.query import QuerySet as _original_QS
+from django.db.models.query_utils import Q as _original_Q
 
-__translation = {
-	'name': '_name',
-	'jname': '_name',
+class Trans:
+	__translation = {
+		'name': '_name', 'jname': '_name',
+		'description': '_description', 'jdetails': '_description',
+		'author': '_author', 'juser_id': '_author',
+		'type': '_type', 'script_id': '_type',
+		'created': '_created', 'staged': '_created',
+		'breeze_stat': '_breeze_stat', 'status': '_status',
+		'rexec': '_rexec', 'rexecut': '_rexec',
+		'dochtml': '_doc_ml', 'docxml': '_doc_ml',
+		'institute': '_institute',
+	}
 
-	'description': '_description',
-	'jdetails': '_description',
+	def has(self, item):
+		if isinstance(item, str) and item != '':
+			text = item
+			if text[0] == '-':
+				text = text[1:]
+			for key in self.__translation.keys():
+				if text.startswith(key):
+					return item.replace(key, self.__translation[key])
+		return None
 
-	'author': '_author',
-	'juser_id': '_author',
+def _translate(args, kwargs):
+	new_arg = list(args)
+	for pos, el in enumerate(new_arg):
+		new_key = Trans().has(el)
+		if new_key is not None:
+			new_arg[pos] = new_key
+	new_arg = tuple(new_arg)
 
-	'type': '_type',
-	'script_id': '_type',
-
-	'created': '_created',
-	'staged': '_created',
-
-	'breeze_stat': '_breeze_stat',
-	'status': '_status',
-
-	'rexec': '_rexec',
-	'rexecut': '_rexec',
-
-	'dochtml': '_doc_ml',
-	'docxml': '_doc_ml',
-}
-
-def _translate(*args, **kwargs):
-	for key in kwargs:
-		if key in __translation.keys():
-			kwargs[__translation[key]] = kwargs[key]
+	for key in kwargs.keys():
+		new_key = Trans().has(key)
+		if new_key is not None:
+			kwargs[new_key] = kwargs[key]
 			del kwargs[key]
-	return args, kwargs
+	return new_arg, kwargs
 
-class Q(__originalQ):
-	"""
-	Overloads original Q class
-	"""
+
+class Q(_original_Q):
 	def __init__(self, *args, **kwargs):
-		super(Q, self).__init__(_translate(*args, **kwargs))
+		args, kwargs = _translate(args, kwargs)
+		super(Q, self).__init__(*args, **kwargs)
 
-# now import QuerySet which should use the new Q class
-from django.db.models.query import QuerySet as __original_QS
-
-class QuerySet(__original_QS):
-	"""
-	Overloads original QuerySet class
-	"""
-	__translate = _translate # writing shortcut
-
-	def annotate(self, *args, **kwargs):
-		super(QuerySet, self).annotate(self.__translate(*args, **kwargs))
+class QuerySet(_original_QS):
+	def _filter_or_exclude(self, negate, *args, **kwargs):
+		args, kwargs = _translate(args, kwargs)
+		return super(QuerySet, self)._filter_or_exclude(negate, *args, **kwargs)
 
 	def filter(self, *args, **kwargs):
-		args, kwargs = self.__translate(*args, **kwargs)
-		super(QuerySet, self).filter(args, kwargs)
+		args, kwargs = _translate(args, kwargs)
+		return super(QuerySet, self).filter(*args, **kwargs)
+
+	def annotate(self, *args, **kwargs):
+		args, kwargs = _translate(args, kwargs)
+		return super(QuerySet, self).annotate(*args, **kwargs)
 
 	def get(self, *args, **kwargs):
-		super(QuerySet, self).get(self.__translate(*args, **kwargs))
+		args, kwargs = _translate(args, kwargs)
+		return super(QuerySet, self).get(*args, **kwargs)
 
 	def exclude(self, *args, **kwargs):
-		super(QuerySet, self).exclude(self.__translate(*args, **kwargs))
+		args, kwargs = _translate(args, kwargs)
+		return super(QuerySet, self).exclude(*args, **kwargs)
 
-# now import QuerySet which should use the new QuerySet class
-from django.db import models
+	def order_by(self, *field_names):
+		field_names, _ = _translate(field_names, dict())
+		return super(QuerySet, self).order_by(*field_names)
 
 class WorkersManager(models.Manager):
 	"""
@@ -73,10 +79,27 @@ class WorkersManager(models.Manager):
 	The Runnable allows to use unified name for fields, while keeping the old database models #TODO migrate
 	Even though, such query should be done from this manager in the future #TODO
 	"""
-
-	def get_queryset(self):
-
+	def get_query_set(self):
 		return QuerySet(self.model, using=self._db)
-		# parent = super(WorkersManager, self)
-		# return QuerySet(parent.model, using=parent._db)
-		# return super(WorkersManager, self).get_queryset()
+
+	def filter(self, *args, **kwargs):
+		args, kwargs = _translate(args, kwargs)
+		return super(WorkersManager, self).filter(*args, **kwargs)
+
+	def annotate(self, *args, **kwargs):
+		args, kwargs = _translate(args, kwargs)
+		return super(WorkersManager, self).annotate(*args, **kwargs)
+
+	def get(self, *args, **kwargs):
+		args, kwargs = _translate(args, kwargs)
+		return super(WorkersManager, self).get(*args, **kwargs)
+
+	def exclude(self, *args, **kwargs):
+		args, kwargs = _translate(args, kwargs)
+		return super(WorkersManager, self).exclude(*args, **kwargs)
+
+	def order_by(self, *args, **kwargs):
+		args, kwargs = _translate(args, kwargs)
+		return super(WorkersManager, self).order_by(*args, **kwargs)
+
+	# TODO : re-write all the queries here
