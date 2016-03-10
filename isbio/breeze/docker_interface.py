f3364a1e0ed8faa2b64ec226c24077c63de55c6b
Clement Fiere <clement@fiere.fr>
2016-03-10 19:16:40 +0200

    milestone, support : images, containers, run; docker daemon is now a parameter Added DockerRun, DockerRepo, DockerContainer

--- isbio/breeze/docker_interface.py
+++ isbio/breeze/docker_interface.py
@@ -1,6 +1,36 @@
 from docker import Client
-from docker.errors import NotFound
+from docker.errors import NotFound, APIError
 from .utils import get_md5, pretty_print_dict_tree
+
+PWD = '.VaQOap_U"@%+D.YQZ[%\')7^}.#Heh?Dq'
+AZURE_REMOTE_URL = 'tcp://127.0.0.1:4243'
+
+
+# clem 10/03/2016
+class DockerRun:
+	image = ''
+	cmd = ''
+
+	def __init__(self, image, cmd=''):
+		assert isinstance(image, (DockerImage, basestring)) and isinstance(cmd, basestring)
+		self.image = image
+		self.cmd = cmd
+
+
+# clem 10/03/2016
+class DockerRepo:
+	url = ''
+	login = ''
+	pwd = ''
+	email = ''
+
+	def __init__(self, login, pwd, email='', url = 'https://index.docker.io'):
+		assert isinstance(login, basestring) and isinstance(pwd, basestring) and isinstance(email, basestring) and \
+			isinstance(url, basestring)
+		self.login = login
+		self.pwd = pwd
+		self.email = email
+		self.url = url
 
 
 # clem 09/03/2016
@@ -16,6 +46,7 @@ class DockerImage:
 	_sig = ''
 
 	def __init__(self, a_dict):
+		assert isinstance(a_dict, dict)
 		self.__dict__.update(a_dict)
 		_ = self.sig
 
@@ -31,20 +62,115 @@ class DockerImage:
 			self._sig = self._get_sig()
 		return self._sig
 
+	# clem 10/03/2016
+	@property
+	def tag(self):
+		if type(self.RepoTags) is list and len(self.RepoTags) > 0:
+			return str(self.RepoTags[0])
+		else:
+			return str(self.RepoTags)
+
 	def __repr__(self):
-		return '<DockerImage %s>' % self.RepoTags[0]
+		return '<DockerImage %s>' % self.tag
+
+	# clem 10/03/2016
+	def __str__(self):
+		return str(self.tag)
+
+
+# clem 09/03/2016
+class DockerContainer:
+	Status = ''
+	Created = 0
+	Labels = dict()
+	Image = DockerImage
+	NetworkSettings = dict()
+	HostConfig = dict()
+	ImageID = ''
+	Command = ''
+	Names = list()
+	Id = ''
+	Ports = list()
+	_sig = ''
+
+	def __init__(self, a_dict, client=None):
+		assert isinstance(a_dict, dict) and (not client or isinstance(client, DockerClient))
+		self.__dict__.update(a_dict)
+		if client: # if we got a client in argument, we chain this container to its DockerImage object
+			try:
+				self.Image = client.images_by_id[self.ImageID]
+			except KeyError:
+				self.Image = a_dict['Image']
+		else:
+			self.Image = a_dict['Image']
+		_ = self.sig
+
+	def _get_sig(self):
+		new_dict = self.__dict__
+		if '_sig' in new_dict:
+			new_dict.pop('_sig')
+		return get_md5(str(new_dict))
+
+	@property
+	def sig(self):
+		if not self._sig:
+			self._sig = self._get_sig()
+		return self._sig
+
+	def __repr__(self):
+		return '<DockerContainer %s>' % self.Names[0]
 
 
 # clem 08/03/2016
 class DockerClient:
 	DEV = False
+	DEBUG = True
+	repo = None
+	default_run = None
 	_raw_cli = None
-	_images = list()
+
+	_daemon_url = ''
+
+	_images_list = list()
 	__image_dict_by_id = dict()
 	__image_tree = dict()
+	_container_list = list()
+	__container_dict_by_id = dict()
 
-	def __init__(self):
-		self._raw_cli = Client(base_url='tcp://127.0.0.1:4243')
+	def __init__(self, repo, daemon_url, run=None): # TODO change the run passing
+		assert isinstance(repo, DockerRepo) and (not run or isinstance(run, DockerRun)) and \
+			isinstance(daemon_url, basestring)
+		self.repo = repo
+		self.default_run = run
+		self._daemon_url = daemon_url
+		self._raw_cli = Client(base_url=daemon_url)
+		self.login()
+
+	# clem 10/03/2016
+	def log(self, obj, force_print=False):
+		if self.DEBUG or force_print:
+			if type(obj) is dict:
+				pretty_print_dict_tree(obj)
+			else:
+				print str(obj)
+		# TODO log
+
+	# clem 10/03/2016
+	def force_log(self, obj):
+		self.log(obj, True)
+
+	# clem 10/03/2016
+	def login(self):
+		if self.repo:
+			try:
+				result = self.cli.login(self.repo.login, self.repo.pwd, self.repo.email, self.repo.url)
+				self.log(result)
+				return 'username' in result or result[u'Status'] == u'Login Succeeded'
+			except APIError as e:
+				self.log(e)
+			except Exception as e:
+				raise e
+		return False
 
 	# clem 09/03/2016
 	@property
@@ -83,17 +209,46 @@ class DockerClient:
 
 		return Prettyfy()
 
+	# clem 10/03/2016
+	def run_default(self):
+		if self.default_run:
+			self._run(self.default_run)
+
 	# clem 09/03/2016
-	def run(self):
+	def img_run(self, img, cmd):
+		return self._run(DockerRun(img, cmd))
+
+	# clem 09/03/2016
+	def _run(self, run):
+		assert isinstance(run, DockerRun)
+
+		# TODO check if connected to repo
+		image_name = str(run.image)
+		if not (type(run.image) is DockerImage or run.image in self.images_by_repo_tag):
+			# image not found, let's try to pull it
+			pass
+
 		# Create the container
 		try:
-			out = self.cli.create_container('fimm/r-light:op')
+			self.log('docker run %s %s' % (image_name, run.cmd))
+			out = self.cli.create_container(image_name, run.cmd)
 			container_id = out['Id']
-			self.cli.start(container_id)
-
+			self.log('Created %s' % container_id)
+			response = self.cli.start(container_id)
+			self.log(response)
+			self.log('Started %s' % container_id)
+			if False:
+				try:
+					# TODO make a proper event listener, also check which events can be read
+					for event in self.cli.events(filters={ u'id': container_id } ):
+						self.force_log(event)
+				except KeyboardInterrupt, StopIteration:
+					pass
+				# print pretty_print_dict_tree(self.cli.inspect_container(container_id))
+			event = self.cli.events(filters={ u'id': container_id } ).next()
+			self.force_log(event)
 		except NotFound as e:
-			raise NotFound(e)
-
+				raise NotFound(e)
 		# If the status code is 404, it means the image doesn't exist:
 
 		# Try to pull it.
@@ -105,7 +260,67 @@ class DockerClient:
 		# Attach to the container, using logs=1 (to have stdout and stderr from the container's start) and stream=1
 
 		# If in detached mode or only stdin is attached, display the container's id.
+		return True
+
+	# clem 10/03/2016
+	@property
+	def ps_by_id(self):
+		"""
+		a dictionary of DockerContainer objects indexed by Id
+		internally containers lists is stored in a dict indexed with containers' Ids.
+		Each time this property is used the dict is refreshed by calling 'docker containers'
+		DockerContainer objects from the cache dict are altered only if container entry changed.
+		DockerContainer objects stores an internal md5 of its dictionary so that a modified container (invariant Id)
+			will be updated
+		similar to images_by_id()
+		:rtype: dict(DockerContainer.Id: DockerContainer)
+		"""
+		# updates the container dict
+		containers = self.cli.containers()
+		images = self.images_by_id # caching, removing that will result in a visible slow down
+		for e in containers:
+			cont = DockerContainer(e)
+			try:
+				cont.Image = images[cont.ImageID]
+			except KeyError:
+				pass
+			if cont.Id not in self.__container_dict_by_id or self.__container_dict_by_id[cont.Id].sig != cont.sig:
+				self.__container_dict_by_id[cont.Id] = cont
+		return self.__container_dict_by_id
+
+	# clem 10/03/2016
+	@property
+	def ps_list(self):
+		"""
+		extracts all DockerContainer objects from containers_by_id to return a list of them
+		:rtype: list(DockerContainer.Id: DockerContainer, )
+		"""
+		self._container_list = list()
+		ids = self.ps_by_id
+		for e in ids:
+			self._container_list.append(ids[e])
+		return self._container_list
+
+	# clem 10/03/2016
+	def show_ps(self):
 		pass
+
+	# clem 10/03/2016
+	@property
+	def ps_by_name(self):
+		"""
+		a dictionary of DockerContainer objects indexed by Name[0]
+		similar to ps_by_id, except here the DockerContainer objects are referenced by their first Names
+		DockerContainer object are referenced from the other dict and thus not modified nor copied.
+		:rtype: dict(DockerContainer.Name: DockerContainer)
+		"""
+		lbl_dict = dict()
+		ids = self.ps_by_id
+		for e in ids:
+			cont = ids[e]
+			lbl_dict[cont.Names[0]] = cont
+
+		return lbl_dict
 
 	# clem 09/03/2016
 	@property
@@ -142,13 +357,13 @@ class DockerClient:
 	def images_list(self):
 		"""
 		extracts all DockerImage objects from images_by_id to return a list of them
-		:rtype: list(DockerImage)
+		:rtype: list(DockerImage.Id: DockerImage, )
 		"""
-		self._images = list()
+		self._images_list = list()
 		ids = self.images_by_id
 		for e in ids:
-			self._images.append(ids[e])
-		return self._images
+			self._images_list.append(ids[e])
+		return self._images_list
 
 	# clem 09/03/2016
 	@property
@@ -160,7 +375,8 @@ class DockerClient:
 		DockerImage objects from the cache dict are altered only if image entry changed.
 		DockerImage objects stores an internal md5 of its dictionary so that a modified image (invariant Id) will be
 			updated
-		:rtype: dict(DockerImage)
+		similar to ps_by_id()
+		:rtype: dict(DockerImage.Id: DockerImage)
 		"""
 		# updates the image dict
 		for e in self.cli.images():
@@ -176,7 +392,7 @@ class DockerClient:
 		a dictionary of DockerImage objects indexed by RepoTag[0]
 		similar to images_by_id, except here the DockerImage objects are referenced by their first RepoTag
 		DockerImage object are referenced from the other dict and thus not modified nor copied.
-		:rtype: dict(DockerImage)
+		:rtype: dict(DockerImage.tag: DockerImage)
 		"""
 		# assert isinstance(self.cli, Client)
 		lbl_dict = dict()
@@ -189,5 +405,7 @@ class DockerClient:
 
 
 def docker():
-	cli = DockerClient()
+	fimm_docker_hub = DockerRepo('fimm', PWD, email='clement.fiere@fimm.fi')
+	fimm_test_run = DockerRun('fimm/r-light:op', './run.sh')
+	cli = DockerClient(fimm_docker_hub, AZURE_REMOTE_URL, fimm_test_run)
 	return cli
