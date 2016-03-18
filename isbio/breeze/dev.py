70349a784f28882eb23636402c2b1deeb023fa02
Clement Fiere <clement@fiere.fr>
2016-03-18 18:56:09 +0200

    fixed docstring for pretty_print

--- isbio/breeze/dev.py
+++ isbio/breeze/dev.py
@@ -1,8 +1,10 @@
+from .utils import advanced_pretty_print as pp
 
 
 def dev():
 	# global client
 	from .docker_interface import Docker
+
 	return Docker()
 
 
@@ -10,7 +12,11 @@ def init():
 	pass
 	# return dev()
 
+
+def same(a, b):
+	return a is b or '%s != %s' % (hex(id(a)), hex(id(b)))
+
 docker = dev()
-docker.self_test()
+# docker.self_test()
 client = docker.client
 
