48ee6e0587817d44f98189c0d8de4a93bb9c1765
Clément Fiere <clement.fiere@helsinki.fi>
2016-05-18 14:25:13 +0300

    restored aledgedly problematic url list

--- isbio/isbio/urls.py
+++ isbio/isbio/urls.py
@@ -86,6 +86,10 @@ else:
 		# url(r'^shiny/apps/((?P<path>[^/]+)/(?P<sub>.*))?$', views.standalone_shiny_in_wrapper),
 		# url(r'^shiny/rep/(?P<rid>\d+)/(?P<path>.*)?$', views.report_shiny_in_wrapper),
 		# url(r'^shiny/libs/(?P<path>.*)$', views.shiny_libs),
+		url(r'^shiny/rep/(?P<rid>\d+)/nozzle$', views.report_file_view_redir),
+		url(r'^shiny/apps/((?P<path>[^/]*)/(?P<sub>.*))?$', views.standalone_shiny_in_wrapper),
+		url(r'^shiny/rep/(?P<rid>\d+)/(?P<path>.*)?$', views.report_shiny_in_wrapper),
+		url(r'^shiny/libs/(?P<path>.*)$', views.shiny_libs),
 		url(r'^runnable/delete/?', views.runnable_del),
 		url(r'^jobs/(?P<page>\d+)?(/)?(?P<state>[a-z]+)?(/)?$', views.jobs),
 		url(r'^jobs/delete/(?P<jid>\d+)(?P<state>[a-z]+)?$', views.delete_job), # FIXME DEPRECATED
