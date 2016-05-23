67d3105bd63b465bf6b1edddf1e8b450f3035ad9
Clément Fiere <clement.fiere@helsinki.fi>
2016-05-23 19:05:34 +0300

    docker container shell script to link container starter and job starter

---
+++ isbio/templates/run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# This script is called after container has extracted the archive.
+# Place here any configuration required and call to the next script which should trigger the job.
+#
+# available_variables	default_value			descritpion
+# IN_FILE		'in.tar.xz'			file name to use for the input/job set archive
+# OUT_FILE		'out.tar.xz'			file name to use fot the output/result archive
+# NEXT_SH		$HOME'/run.sh'			the path of this file once extracted into the container
+# AZURE_PY		$RES_FOLDER'/'$AZURE_STORAGE_FN	full path of the azure-storage python interface
+# JOB_ID						the ID of the job, the only data passed on to the container (usually the md5 of the job archive)
+# HOME			/root				home folder of the container. This is where all operations will happend
+# HOSTNAME						the hostname of the container. usually the 12 fisrt char of the docker-container id
+# JAVA_HOME		/usr/java/jdk1.8.0_72/		the path to java in the container
+# R_LIBS_USER 		/usr/lib/R/site-library t	the path to R in the container
+# USER			root				current linux user inside the container. will ALWAYS be root
+## BREEZE AUTO configuration
+ABS_PATH=$job_full_path
+## END OF BREEZE_CONFIG
+export JOB_FOLDER=${HOME}${ABS_PATH}  # this is Breeze implementation specific and MUST be changed
+export NEXT_SH=./'$run_job_sh'
+export OUT_FILE_PATH=${HOME}'/'${OUT_FILE}
+
+END_C='\033[39m'
+BLUE='\033[34m'
+RED='\033[31m'
+# move to the job sub folder
+echo -e ${BLUE}'cd '${JOB_FOLDER}''${END_C}
+cd ${JOB_FOLDER}
+# bootstrap
+echo -e ${BLUE}'Running '${NEXT_SH}' ...'${END_C}
+${NEXT_SH}
+EX=$?
+if [ $EX -eq 0 ];
+then
+	# chown -R 1001:1001 *
+	# preventive delete (suppressing errors)
+	rm ${OUT_FILE_PATH} > /dev/null 2>&1
+	echo -ne ${BLUE}'Creating archive '${OUT_FILE_PATH}' ...'${END_C} && echo 'done'
+	tar Jcf ${OUT_FILE_PATH} .
+	${AZURE_PY}' save'
+	echo 'done'
+fi
+exit ${EX}
