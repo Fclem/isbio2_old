35ca3f04454b7c34e4a35c3f156ede668392ff58
Clément Fiere <clement.fiere@helsinki.fi>
2016-06-17 14:01:02 +0300

    trying to fix proxy issue with non-ASCII file upload (still broken)

--- isbio/breeze/auxiliary.py
+++ isbio/breeze/auxiliary.py
@@ -478,17 +478,52 @@ def proxy_to(request, path, target_url, query_s='', silent=False, timeout=None):
 		qs = '?' + request.META['QUERY_STRING']
 		url += qs
 	opener = urllib2.build_opener()
-	data = u""
+	data = ''
 	if request.method == 'POST':
+
 		from urllib import quote_plus
 
-		def urlencode_utf8(params):
-			if hasattr(params, 'items'):
-				params = params.items()
-			return '&'.join(
-				(quote_plus(k.encode('utf8'), safe='/') + '=' + quote_plus(v.encode('utf8'), safe='/')
-					for k, v in params))
-		data = urlencode_utf8(dict(request.POST.iteritems())) # data should be bytes
+		print 'POST:', str(request.POST)
+
+		def encode_obj(in_obj):
+
+			def encode_list(in_list):
+				out_list = []
+				for el in in_list:
+					out_list.append(encode_obj(el))
+				return out_list
+
+			def encode_dict(in_dict):
+				out_dict = { }
+				for k, v in in_dict.iteritems():
+					out_dict[k.encode('utf-8')] = encode_obj(v)
+				return out_dict
+
+			if isinstance(in_obj, unicode):
+				return in_obj.encode('utf-8')
+			elif isinstance(in_obj, list):
+				return encode_list(in_obj)
+			elif isinstance(in_obj, tuple):
+				return tuple(encode_list(in_obj))
+			elif isinstance(in_obj, dict):
+				return encode_dict(in_obj)
+
+			return in_obj
+		#import requests
+		#payload_str = "&".join("%s=%s" % (k, v) for k, v in dict(request.POST.iteritems()))
+		## req = requests.post(url, params=payload_str)
+		#req = requests.get(url, params=payload_str)
+		#return HttpResponse(req.content, status=req.status_code, mimetype=req.apparent_encoding)
+
+		tmp = encode_obj(dict(request.POST.iteritems()))
+		#from cStringIO import StringIO
+		#buf = StringIO()
+		#for k, v in tmp.iteritems():
+		#	buf.write(k + '=' + v + '&')
+
+		# data = buf.getvalue()[:-1]
+		data = '&'.join(k + '=' + v for k, v in tmp.iteritems())  # data should be bytes
+		#print '#' * 56 + ' DATA : ' + '#' * 56 + '\n' + data[:1024] + '\n' + '#' * 120
 
 	log = '/var/log/shiny-server.log'
 	log_size = os.stat(log).st_size
